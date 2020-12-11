24bff21d0a95c936f0d804535f1e8f4edcdecff2
Sudhir Voleti <sudhir_voleti@isb.edu>
2020-12-11 15:48:29 +0530

    Update dependency-segmentation-discriminant-and-targeting-shinyapp.R

--- dependency-segmentation-discriminant-and-targeting-shinyapp.R
+++ dependency-segmentation-discriminant-and-targeting-shinyapp.R
@@ -1,16 +1,16 @@
-if (!require("shiny")} {install.packages("shiny")};library(shiny)
-if (!require("devtools")} {install.packages("devtools")}; library(devtools)
-if (!require("ggbiplot")} {install_github("ggbiplot","vqv")}
-if (!require("ggplot2")} {install.packages("ggplot2")}
-if (!require("dplyr")} {install.packages("dplyr")}
+if (!require("shiny")) {install.packages("shiny")};library(shiny)
+if (!require("devtools")) {install.packages("devtools")}; library(devtools)
+if (!require("ggbiplot")) {install_github("ggbiplot","vqv")}
+if (!require("ggplot2")) {install.packages("ggplot2")}
+if (!require("dplyr")) {install.packages("dplyr")}
 # if (!require("plyr")} {install.packages("plyr")}
-if (!require("scales")} {install.packages("scales")}
-if (!require("grid")} {install.packages("grid")}; library("grid")
-if (!require("cluster")} {install.packages("cluster")}
-if (!require("mclust")} {install.packages("mclust")}
-if (!require("MASS")} {install.packages("MASS")}
-if (!require("scales")} {install.packages("scales")}
-if (!require("gridExtra")} {install.packages("gridExtra")}
+if (!require("scales")) {install.packages("scales")}
+if (!require("grid")) {install.packages("grid")}; library("grid")
+if (!require("cluster")) {install.packages("cluster")}
+if (!require("mclust")) {install.packages("mclust")}
+if (!require("MASS")) {install.packages("MASS")}
+if (!require("scales")) {install.packages("scales")}
+if (!require("gridExtra")) {install.packages("gridExtra")}
 
 library("shiny")
 library("cluster")
